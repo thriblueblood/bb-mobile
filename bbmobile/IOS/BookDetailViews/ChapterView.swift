a09f9c16ac8c49054fef11096761bead558d21f5
thriblueblood <60379410+thriblueblood@users.noreply.github.com>
2021-04-23 18:53:03 +0700

    firebase added home

--- bbmobile/IOS/BookDetailViews/ChapterView.swift
+++ bbmobile/IOS/BookDetailViews/ChapterView.swift
@@ -11,32 +11,38 @@ import Kingfisher
 struct ChapterView: View {
     var episode : [Episode]
     var body: some View {
-        HStack {
-            VStack(alignment:.leading) {
-                    ForEach(episode) { e in
-                        Button(action: {
-                            
-                        }, label: {
-                            VStack(alignment:.leading){
-                                HStack{
-                                    EpisodeImgView(imageURL: e.Image)
-                                        .frame(width:150,height:100)
-                                        .clipped()
-                                    VStack{
-                                        Text(e.name)
-                                            .foregroundColor(Color("CustomWhite"))
-                                            .font(.custom("Lato-Bold", size: 16))
-                                    }
-                                }
-
-                            }
-                        })
-
-                    }
-                    Spacer()
-            }
-            Spacer()
+        VStack{
+            KFImage(URL(string: "https://firebasestorage.googleapis.com/v0/b/bb-mobile-2fa59.appspot.com/o/my_hero_277%233.png?alt=media&token=ee891ced-6304-40c9-acdc-988aa7e37e1b")!)
+                .resizable()
+                .scaledToFit()
+                .padding()
         }
+//        HStack {
+//            VStack(alignment:.leading) {
+//                    ForEach(episode) { e in
+//                        Button(action: {
+//
+//                        }, label: {
+//                            VStack(alignment:.leading){
+//                                HStack{
+//                                    EpisodeImgView(imageURL: e.Image)
+//                                        .frame(width:150,height:100)
+//                                        .clipped()
+//                                    VStack{
+//                                        Text(e.name)
+//                                            .foregroundColor(Color("CustomWhite"))
+//                                            .font(.custom("Lato-Bold", size: 16))
+//                                    }
+//                                }
+//
+//                            }
+//                        })
+//
+//                    }
+//                    Spacer()
+//            }
+//            Spacer()
+//        }
 
     }
 }
